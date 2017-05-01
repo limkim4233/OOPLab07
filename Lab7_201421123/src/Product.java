d7cab6b21927b3129c5140ace2722f4fb973fc88
limkim4233 <limkim4233@gmail.com>
2017-05-01 11:05:08 +0900

    Product Class

---
+++ Lab7_201421123/src/Product.java
@@ -0,0 +1,16 @@
+
+public abstract class Product {
+
+	private double price;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public abstract String getName();	
+	
+}
