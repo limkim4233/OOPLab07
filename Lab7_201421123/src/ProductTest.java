406d5db05003be058d86f4823596e249ff060c8c
ChangSeongBeom <iwsl1234@naver.com>
2017-05-03 23:06:34 +0900

    saf

--- Lab7_201421123/src/ProductTest.java
+++ Lab7_201421123/src/ProductTest.java
@@ -4,21 +4,30 @@ public class ProductTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		ProductTest test=new ProductTest();
 		Product  tv=new SmartTV();
-	
-		
-		
+	    Product  laptop=new Laptop();
+	    Product  t=new Toaster();
+	    
+	    test.testProduct(tv);
+		test.testProduct(laptop);
+		test.testProduct(t);
 
 	}
+	{
+		
+	}
 	
-	public void testDataStorage(DataStorage d)	
+	public void testDataStorage(SmartTV t)	
 	{
 		
 	}
 	public void testProduct(Product p)
 	{
-		
+	      p.setPrice(20000);
+	      System.out.println(p.getPrice());
+	      System.out.println(p.getName());
+	      
 	}
 	
 
